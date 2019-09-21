c762b4465bfbbbce97fbdf47d64076b41e0f0a82
Bruce Gustin <bgustin@srcs.org>
2019-09-21 15:59:28 -0700

    Initial commit

---
+++ Assets/Scripts/OpenScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenScene : MonoBehaviour
+{
+    public void switchScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
